9445e2149b412612593e85da015aed0efff6b940
NataCoimbra <nsilvac1930@gmail.com>
2021-06-14 23:51:47 -0300

    I created the function to convert bin to dec

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
 
 
 function convertBinCode(){
+    var binCode = document.querySelector("#binCode");
+    document.querySelector("#convertedNumber").innerHTML = bin2dec(binCode.value);
+}
+
+
+function bin2dec(binCode){
+
+    var dec = 0;
+
+    for(var i = binCode.length - 1; i >= 0; i--){
+        dec += binCode[i] * (2 ** (binCode.length - 1 - i));
+    }
+
+    return dec;
 
 }
